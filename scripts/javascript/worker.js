25d6c31c774c63ff8f6d61ac576fb1ca4434053b
花楹一间 <yanl_802@163.com>
2025-04-23 11:23:49 +0800

    chore: 删除 husky 和 commitlint 配置

---
+++ scripts/javascript/worker.js
@@ -0,0 +1,7 @@
+// 在主线程中使用 worker.onmessage，在 Worker 中使用 self.onmessage。
+self.onmessage = function(event) {
+    const data = event.data;
+    const result = data * 2; 
+    self.postMessage(result);
+    // throw new Error('Something went wrong!');
+};
