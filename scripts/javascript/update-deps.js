a9b19910de4ab38ca7c950b812f8d8a48985c6ef
花楹一间 <yanl_802@163.com>
2025-05-14 23:56:56 +0800

    feat(scripts): 添加自动检测过时依赖的脚本
    - 新增 update-deps.js 脚本，用于自动检测和提示过时的依赖包
    - 通过 npm outdated 命令获取过时依赖信息，并过滤掉需要固定的依赖包
    - 以友好的格式输出更新提示，包括所有可更新的依赖包版本

---
+++ scripts/javascript/update-deps.js
@@ -0,0 +1,33 @@
+// update-deps.js
+const { exec } = require("node:child_process")
+
+/**
+ * 忽略的依赖包 - 版本差异过大，更改后会造成项目崩坏
+ * @type {string[]}
+ */
+const fixedDeps = [
+  // 'tailwindcss'
+]
+
+try {
+  const subprocess = exec("npm outdated --json", {
+    encoding: "utf-8"
+  })
+
+  subprocess.stdout.on("data", data => {
+    const outdated = JSON.parse(data)
+    const pkgs = Object.keys(outdated).filter(pkg => !fixedDeps.includes(pkg))
+    console.log("Checking dependency updates...", outdated)
+    if (!pkgs.length) return
+    const tips = pkgs.map(pkg => `${pkg}@latest`).join(" ")
+    const start_char = "\n".padStart(50, "*")
+    const end_char = "\n".padEnd(50, "*")
+    console.log(
+      start_char,
+      `Update all dependencies to the latest version:\nnpm install ${tips}`,
+      end_char
+    )
+  })
+} catch (error) {
+  console.error("Script execution error:", error.status, error.message)
+}
